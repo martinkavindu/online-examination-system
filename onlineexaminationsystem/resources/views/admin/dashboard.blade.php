79fd7ae55fd73b7fba0349344c80ebdb934e27e2
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-01 19:21:56 +0300

    m

--- onlineexaminationsystem/resources/views/admin/dashboard.blade.php
+++ onlineexaminationsystem/resources/views/admin/dashboard.blade.php
@@ -14,6 +14,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script type="text/javascript" src="{{ asset('js/multiselect-dropdown.js') }}"></script>
+
     <title>admin dashboard</title>
 </head>
 
--- onlineexaminationsystem/resources/views/exam/questions.blade.php
+++ onlineexaminationsystem/resources/views/exam/questions.blade.php
@@ -11,10 +11,13 @@
         <div class="mb-3">
             <input type="hidden" class="form-control" id="exam_id" name="exam_id" placeholder="exam id"  required>
         </div>
-
-        <div class="container mt-5">
-            <select class="selectpicker" multiple aria-label="Default select example" data-live-search="true">
-              <option value="1">One</option>
+        <div class="mb-3">
+        <label class="text-white"> Select questions</label>
+        </div>
+        <div class="container mb-3">
+            <select class="form-control form-select" name="questions" multiple multiselect-search='true'
+            multiselect-select-all="true" multiselect-max-items="20" placeholder='select question'>
+              <option value=""> one</option>
               <option value="2">Two</option>
               <option value="3">Three</option>
               <option value="4">Four</option>
