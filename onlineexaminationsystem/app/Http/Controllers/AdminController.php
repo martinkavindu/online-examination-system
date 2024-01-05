89416726b38a494d427baab0e71c2e048ca13174
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-05 08:46:00 +0300

    m

--- onlineexaminationsystem/app/Http/Controllers/AdminController.php
+++ onlineexaminationsystem/app/Http/Controllers/AdminController.php
@@ -335,7 +335,7 @@ return view('exam.edit_exam',compact('exams'));
         public function AllQuestions($id)
         {
             $exam = QnaExam::findOrFail($id);
-            $questions = Question::findOrFail($exam->question_id);
+            $questions = $exam->question_id;
         
             return view('exam.allquestions', compact('exam', 'questions'));
         }
